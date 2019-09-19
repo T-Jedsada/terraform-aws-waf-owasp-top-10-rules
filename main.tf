25e4580df16aa5b029ec7320225c82cd1e7a834d
Jedsada Tiwongvorakul <jt@20scoops.net>
2019-09-19 22:50:36 +0700

    update config

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
 # Random ID Generator
 
 provider "aws" {
-  region  = "ap-southeast-1"
+  region  = "${var.region}"
   version = "~> 2.7"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
 }
 
 resource "random_id" "this" {
